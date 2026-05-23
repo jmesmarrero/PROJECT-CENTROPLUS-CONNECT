ca92aa963622d8de11804ab3d837773253229741
Jorge Mesa Marrero <jorgemm.dam@gmail.com>
2026-05-23 12:26:09 +0100

    feat: add UsuarioRepository full implementation

--- backend-api/src/main/java/es/ies/puerto/repositories/sqlite/UsuarioRepository.java
+++ backend-api/src/main/java/es/ies/puerto/repositories/sqlite/UsuarioRepository.java
@@ -3,21 +3,23 @@ package es.ies.puerto.repositories.sqlite;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
+import es.ies.puerto.models.TipoUsuario;
 import es.ies.puerto.models.Usuario;
 import es.ies.puerto.repositories.IUsuarioRepository;
 
 public class UsuarioRepository extends SQLiteConnectionManager implements IUsuarioRepository {
 
-    protected UsuarioRepository(String databaseUrl) {
+    public UsuarioRepository(String databaseUrl) {
         super(databaseUrl);
 
     }
 
     @Override
     public boolean create(Usuario usuario) {
-        String sql = "INSERT INTO usuario (nombre, dni, email, telefono, tipo_usuario) VALUES (?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO usuarios (nombre, dni, email, telefono, tipo_usuario) VALUES (?, ?, ?, ?, ?)";
         try (Connection connection = this.getConnection();
                 PreparedStatement sentencia = connection.prepareStatement(sql)) {
             sentencia.setString(1, usuario.getNombre());
@@ -36,26 +38,118 @@ public class UsuarioRepository extends SQLiteConnectionManager implements IUsuar
 
     @Override
     public Usuario findById(Long id) {
+        String sql = "SELECT * FROM usuarios WHERE id = ?";
+        try (Connection connection = this.getConnection();
+                PreparedStatement sentencia = connection.prepareStatement(sql)) {
+            sentencia.setLong(1, id);
+            ResultSet resultado = sentencia.executeQuery();
+
+            while (resultado.next()) {
+
+                String nombre = resultado.getString("nombre");
+                String dni = resultado.getString("dni");
+                String email = resultado.getString("email");
+                String telefono = resultado.getString("telefono");
+                TipoUsuario tipoUsuario = TipoUsuario.valueOf(resultado.getString("tipo_usuario"));
+
+                return new Usuario(id, nombre, dni, email, telefono, tipoUsuario);
+
+            }
+
+        } catch (Exception e) {
+            System.err.println("No se ha podido encontrar Usuario por id: " + id);
+        }
         return null;
     }
 
     @Override
     public List<Usuario> findAll() {
-        return null;
+        List<Usuario> usuarios = new ArrayList<>();
+        String sql = "SELECT * FROM usuarios";
+
+        try (Connection connection = this.getConnection();
+                PreparedStatement sentencia = connection.prepareStatement(sql)) {
+            ResultSet resultado = sentencia.executeQuery();
+            while (resultado.next()) {
+
+                Long id = resultado.getLong("id");
+                String nombre = resultado.getString("nombre");
+                String dni = resultado.getString("dni");
+                String email = resultado.getString("email");
+                String telefono = resultado.getString("telefono");
+                TipoUsuario tipoUsuario = TipoUsuario.valueOf(resultado.getString("tipo_usuario"));
+
+                Usuario usuario = new Usuario(id, nombre, dni, email, telefono, tipoUsuario);
+                usuarios.add(usuario);
+            }
+            return usuarios;
+
+        } catch (Exception e) {
+            System.err.println("No se han encontrado usuarios");
+            return new ArrayList<>();
+        }
     }
 
     @Override
     public boolean update(Usuario usuario) {
-        return false;
+
+        String sql = "UPDATE usuarios SET nombre=?, dni=?, email=?, telefono=?, tipo_usuario=? WHERE id = ?";
+        try (Connection connection = this.getConnection();
+                PreparedStatement sentencia = connection.prepareStatement(sql)) {
+
+            sentencia.setString(1, usuario.getNombre());
+            sentencia.setString(2, usuario.getDni());
+            sentencia.setString(3, usuario.getEmail());
+            sentencia.setString(4, usuario.getTelefono());
+            sentencia.setString(5, usuario.getTipoUsuario().name());
+            sentencia.setLong(6, usuario.getId());
+
+            return sentencia.executeUpdate() > 0;
+
+        } catch (Exception e) {
+            System.err.println("No se ha podido actualizar usuario: " + usuario);
+            return false;
+        }
+
     }
 
     @Override
     public boolean deleteById(Long id) {
-        return false;
+        String sql = "DELETE FROM usuarios WHERE id = ?";
+        try (Connection connection = this.getConnection();
+                PreparedStatement sentencia = connection.prepareStatement(sql)) {
+            sentencia.setLong(1, id);
+
+            return sentencia.executeUpdate() > 0;
+
+        } catch (Exception e) {
+            System.err.println("No se ha podido eliminar usuario con id: " + id);
+            return false;
+        }
+
     }
 
     @Override
     public Usuario findByEmail(String email) {
+        String sql = "SELECT * FROM usuarios WHERE email = ?";
+        try (Connection connection = this.getConnection();
+                PreparedStatement sentencia = connection.prepareStatement(sql)) {
+            sentencia.setString(1, email);
+            ResultSet resultado = sentencia.executeQuery();
+            while (resultado.next()) {
+
+                Long id = resultado.getLong("id");
+                String nombre = resultado.getString("nombre");
+                String dni = resultado.getString("dni");
+                // String email = resultado.getString("email");
+                String telefono = resultado.getString("telefono");
+                TipoUsuario tipoUsuario = TipoUsuario.valueOf(resultado.getString("tipo_usuario"));
+
+                return new Usuario(id, nombre, dni, email, telefono, tipoUsuario);
+            }
+        } catch (Exception e) {
+            System.err.println("no se pudo encontrar usuario por email: " + email);
+        }
         return null;
     }
 
